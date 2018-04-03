4b9579fecf6671c35242863bd98b680e5a0872bc
yu <hengchen2005@gmail.com>
2018-04-04 00:14:09 +0800

    code refactoring

---
+++ fuckpy/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author: yushuibo
+@Copyright (c) 2018 yushuibo. All rights reserved.
+@Licence: GPL-2
+@Email: hengchen2005@gmail.com
+@File: setup.py
+@Create: 2018-04-03 23:49:42
+@Last Modified: 2018-04-03 23:49:42
+@Desc: --
+"""
+
+
+
+from distutils.core import setup
+
+setup(name='fuckpy',
+      version='1.0',
+      author='yushuibo',
+      author_email='hengchen2005@gmail.com',
+      utl='https://github.com/yushuibo/fuckpy',
+      packages=['fuckpy'],
+      )
